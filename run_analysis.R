7114a2297100b89c06764ed2c649c06ebb693191
FranSal <rubycscf@me.com>
2015-02-20 18:10:29 +0100

    v1.3
    R script for the exercise

--- run_analysis.R
+++ run_analysis.R
@@ -19,21 +19,22 @@ trainyfile <- "y_train.txt"
 subjTr <- "subject_train.txt"
 subjTs <- "subject_test.txt"
 
-## reading train set . First we paste the fie dir wirh the name of the file
+## reading x_train set . First we paste the fie dir wirh the name of the file
 path1 <- paste(traindir,"/",trainxfile, sep="")
 raw_train <- read.fwf(path1, widths =rep(16,561), header = FALSE,   numerals = "no.loss", n = -1) # reads fixed width X_train.txt file
 
-# now it reads Y_train.txt after setting its path to path12 using read.fwf() seting its widths to 1 and header =FALSE
+# now it reads y_train.txt after setting its path to path12 using read.fwf() seting its widths to 1 and header =FALSE
 # same idea for reading subject_train.txt but setting the width = 2 because we have up to 30 subject id's, (from 1 to 30).
 path12 <- paste(traindir,"/",trainyfile, sep="") 
 y_train <- read.fwf(path12, widths =1, header = FALSE) 
+# reading subject_train.txt
 path13 <- paste(traindir,"/",subjTr, sep="")
 subj_train <- read.fwf(path13, widths =2, header = FALSE)
 
-## reading test set
+## reading x_test set
 path2 <- paste(testdir,"/", testxfile, sep="")
 raw_test <- read.fwf(path2, widths =rep(16,561), header = FALSE,   numerals = "no.loss", n = -1) # reads fixed width  X_test.txt file
-# reads Y_test.txt
+# reads y_test.txt
 path22 <- paste(testdir,"/",testyfile, sep="")
 y_test <- read.fwf(path22, widths =1, header = FALSE)
 # reading subject_test.txt
@@ -53,29 +54,30 @@ subjtest <- tbl_df(subj_test)
 total_x <-bind_rows(xtrain,xtest)
 total_y <-bind_rows(ytrain,ytest)
 total_subj <- bind_rows(subjtrain,subjtest)
+
 names(total_subj) <- "subject"  ## changing the label of total_subj to a useful one.
 
-# reading thee features.txt file to extract the indexes we are interested in 
+# reading the features.txt file to extract the indexes we are interested in 
 tmp <- read.table("features.txt",header= FALSE, sep=" ", nrows=561)
-features <- tbl_df(tmp[ grepl("mean()",tmp$V2) | grepl("std()",tmp$V2),])
- ## Now wr extract to a custom table the columns we are interested in from the merged total_x data set.
+features <- tbl_df(tmp[ grepl("mean()",tmp$V2) | grepl("std()",tmp$V2),]) # we are interested only in those with text including "mean()" or "std()" 
+
+ ## Now we extract  the columns we are interested in from the merged total_x data set -> to a custom table ( my_total_x).
 my_total_x <- select(total_x, features$V1)
 
-## now we can name the columns with the names of the features taken from features$V2 
+## now we  name the columns with the names of the features taken from features$V2 
 my_total_x <- setNames(my_total_x, features$V2) 
 
 ## Reading the activity_labels.txt to extract the names of the activites. 
-## This file it's assumed to be present in the main Data-Cleaning-Course-Project folder
+## This file it's assumed to be present on the base dir  (Data-Cleaning-Course-Project folder)
 activities <- read.table("activity_labels.txt",header= FALSE, sep=" ", nrows=6)
 
 ## mutating total_y by changing its numeric code to an activity name
 my_total_y <- total_y %>% 
-                mutate( activity = activities$V2[total_y$V1]) %>%
-                select(activity)
+                mutate( activity = activities$V2[total_y$V1]) %>% # here we are adding a new column named "activity"
+                select(activity) # with select() we'll get rid of the the old V1 col we inherited from the original total_y
 
-# finally merging together the three data sets, putting "subject" and activity in the fists two columns.
+# finally merging together the three data sets, putting "subject" and activity as the firts two columns.
 my_dataset <- tbl_df(bind_cols(total_subj, my_total_y, my_total_x ))
-    
     
 ## Now we'll create a separate data set with the average of each variable by subject and activity
 averages_set <- my_dataset %>%  
